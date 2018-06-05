5dca0b9775fb26a99585d9316c5e34a4e454199d
megha.m <megha.m@github.com>
2018-06-05 17:44:23 +0530

    httpp, routing added

---
+++ index.js
@@ -0,0 +1,20 @@
+//Main starting point of application
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const router = require('./router.js')
+//app setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+//server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on', port);
+
